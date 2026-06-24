50753c50f7abbb9e6ff3b37e63bcffe183cf9a4a
trysetgo <trysetgo@gmail.com>
2026-06-24 23:22:13 +0530

    CMS Sync: 2026-06-24T17:52:13.385Z

--- src/components/LayoutRenderer.jsx
+++ src/components/LayoutRenderer.jsx
@@ -1,45 +1,34 @@
 import React from "react";
-import CenteredCta from "./cms/CenteredCta.jsx";
+import Paragraph from "./cms/Paragraph.jsx";
+import MinimalHeader from "./cms/MinimalHeader.jsx";
+import Heading from "./cms/Heading.jsx";
+import List from "./cms/List.jsx";
+import TimelineRoadmap from "./cms/TimelineRoadmap.jsx";
 import Footer from "./cms/Footer.jsx";
 import Header from "./cms/Header.jsx";
-import Heading from "./cms/Heading.jsx";
 import HeroBanner from "./cms/HeroBanner.jsx";
-import IllustratedHowItWorks from "./cms/IllustratedHowItWorks.jsx";
-import ImageCard from "./cms/ImageCard.jsx";
-import LayoutSelector from "./cms/LayoutSelector.jsx";
-import List from "./cms/List.jsx";
-import MinimalHeader from "./cms/MinimalHeader.jsx";
-import Paragraph from "./cms/Paragraph.jsx";
-import RichTextContentBlock from "./cms/RichTextContentBlock.jsx";
-import TimelineRoadmap from "./cms/TimelineRoadmap.jsx";
-import TrendingProductHero from "./cms/TrendingProductHero.jsx";
 import WhyChooseOurBrand from "./cms/WhyChooseOurBrand.jsx";
+import TrendingProductHero from "./cms/TrendingProductHero.jsx";
+import IllustratedHowItWorks from "./cms/IllustratedHowItWorks.jsx";
+import CenteredCta from "./cms/CenteredCta.jsx";
+import ImageCard from "./cms/ImageCard.jsx";
+import RichTextContentBlock from "./cms/RichTextContentBlock.jsx";
 
 const LayoutRendererInternalComponentMap = {
-  CenteredCta: CenteredCta,
-  "Centered Cta": CenteredCta,
+  Paragraph: Paragraph,
+  "Minimal Header": MinimalHeader,
+  Heading: Heading,
+  List: List,
+  "Timeline / Roadmap": TimelineRoadmap,
   Footer: Footer,
   Header: Header,
-  Heading: Heading,
-  HeroBanner: HeroBanner,
   "Hero Banner": HeroBanner,
-  IllustratedHowItWorks: IllustratedHowItWorks,
-  "Illustrated How It Works": IllustratedHowItWorks,
-  ImageCard: ImageCard,
-  "Image Card": ImageCard,
-  LayoutSelector: LayoutSelector,
-  List: List,
-  MinimalHeader: MinimalHeader,
-  "Minimal Header": MinimalHeader,
-  Paragraph: Paragraph,
-  RichTextContentBlock: RichTextContentBlock,
-  "Rich Text Content Block": RichTextContentBlock,
-  TimelineRoadmap: TimelineRoadmap,
-  "Timeline Roadmap": TimelineRoadmap,
-  TrendingProductHero: TrendingProductHero,
-  "Trending Product Hero": TrendingProductHero,
-  WhyChooseOurBrand: WhyChooseOurBrand,
   "Why Choose Our Brand": WhyChooseOurBrand,
+  "Trending Product Hero": TrendingProductHero,
+  "Illustrated How It Works": IllustratedHowItWorks,
+  "Centered CTA": CenteredCta,
+  "Image Card": ImageCard,
+  "Rich Text Content Block": RichTextContentBlock,
 };
 
 let RenderElementInternal;
